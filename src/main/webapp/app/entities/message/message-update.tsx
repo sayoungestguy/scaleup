de736c3413739f6a64a808d69820a3dff8c151ca
Lee Wei Jie <weijie899@hotmail.com>
2024-09-25 01:27:00 +0800

    disable edit/delete button if receiver profile id matches logged in user's id.
    disabled sender profile field, will retrieve logged in user's id via api

--- src/main/webapp/app/entities/message/message-update.tsx
+++ src/main/webapp/app/entities/message/message-update.tsx
@@ -1,3 +1,89 @@
+// import React, { useState, useEffect } from 'react';
+// import { Link, useNavigate, useParams } from 'react-router-dom';
+// import { Button, Row, Col, FormText } from 'reactstrap';
+// import { isNumber, ValidatedField, ValidatedForm } from 'react-jhipster';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+// import { convertDateTimeFromServer, convertDateTimeToServer, displayDefaultDateTime } from 'app/shared/util/date-utils';
+// import { mapIdList } from 'app/shared/util/entity-utils';
+// import { useAppDispatch, useAppSelector } from 'app/config/store';
+
+// import { IUserProfile } from 'app/shared/model/user-profile.model';
+// import { getEntities as getUserProfiles } from 'app/entities/user-profile/user-profile.reducer';
+// import { IMessage } from 'app/shared/model/message.model';
+// import { getEntity, updateEntity, createEntity, reset } from './message.reducer';
+
+// export const MessageUpdate = () => {
+//   const dispatch = useAppDispatch();
+
+//   const navigate = useNavigate();
+
+//   const { id } = useParams<'id'>();
+//   const isNew = id === undefined;
+
+//   const userProfiles = useAppSelector(state => state.userProfile.entities);
+//   const messageEntity = useAppSelector(state => state.message.entity);
+//   const loading = useAppSelector(state => state.message.loading);
+//   const updating = useAppSelector(state => state.message.updating);
+//   const updateSuccess = useAppSelector(state => state.message.updateSuccess);
+
+//   const handleClose = () => {
+//     navigate('/message');
+//   };
+
+//   useEffect(() => {
+//     if (!isNew) {
+//       dispatch(getEntity(id));
+//     }
+
+//     dispatch(getUserProfiles({}));
+//   }, []);
+
+//   useEffect(() => {
+//     if (updateSuccess) {
+//       handleClose();
+//     }
+//   }, [updateSuccess]);
+
+//   // eslint-disable-next-line complexity
+//   const saveEntity = values => {
+//     if (values.id !== undefined && typeof values.id !== 'number') {
+//       values.id = Number(values.id);
+//     }
+//     values.sentAt = convertDateTimeToServer(values.sentAt);
+//     values.createdDate = convertDateTimeToServer(values.createdDate);
+//     values.lastModifiedDate = convertDateTimeToServer(values.lastModifiedDate);
+
+//     const entity = {
+//       ...messageEntity,
+//       ...values,
+//       senderProfile: userProfiles.find(it => it.id.toString() === values.senderProfile?.toString()),
+//       receiverProfile: userProfiles.find(it => it.id.toString() === values.receiverProfile?.toString()),
+//     };
+
+//     if (isNew) {
+//       dispatch(createEntity(entity));
+//     } else {
+//       dispatch(updateEntity(entity));
+//     }
+//   };
+
+//   const defaultValues = () =>
+//     isNew
+//       ? {
+//           sentAt: displayDefaultDateTime(),
+//           createdDate: displayDefaultDateTime(),
+//           lastModifiedDate: displayDefaultDateTime(),
+//         }
+//       : {
+//           ...messageEntity,
+//           sentAt: convertDateTimeFromServer(messageEntity.sentAt),
+//           createdDate: convertDateTimeFromServer(messageEntity.createdDate),
+//           lastModifiedDate: convertDateTimeFromServer(messageEntity.lastModifiedDate),
+//           senderProfile: messageEntity?.senderProfile?.id,
+//           receiverProfile: messageEntity?.receiverProfile?.id,
+//         };
+
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import { Button, Row, Col, FormText } from 'reactstrap';
@@ -12,12 +98,11 @@ import { IUserProfile } from 'app/shared/model/user-profile.model';
 import { getEntities as getUserProfiles } from 'app/entities/user-profile/user-profile.reducer';
 import { IMessage } from 'app/shared/model/message.model';
 import { getEntity, updateEntity, createEntity, reset } from './message.reducer';
+import axios from 'axios';
 
 export const MessageUpdate = () => {
   const dispatch = useAppDispatch();
-
   const navigate = useNavigate();
-
   const { id } = useParams<'id'>();
   const isNew = id === undefined;
 
@@ -27,17 +112,28 @@ export const MessageUpdate = () => {
   const updating = useAppSelector(state => state.message.updating);
   const updateSuccess = useAppSelector(state => state.message.updateSuccess);
 
+  const [currentUserId, setCurrentUserId] = useState(null);
+
   const handleClose = () => {
     navigate('/message');
   };
 
   useEffect(() => {
+    const fetchCurrentUserId = async () => {
+      try {
+        const response = await axios.get('/api/current-user'); // Adjust this endpoint if necessary
+        setCurrentUserId(response.data); // Assuming response.data contains the user ID
+      } catch (error) {
+        console.error('Error fetching current user ID:', error);
+      }
+    };
+
+    fetchCurrentUserId();
     if (!isNew) {
       dispatch(getEntity(id));
     }
-
     dispatch(getUserProfiles({}));
-  }, []);
+  }, [dispatch, id, isNew]);
 
   useEffect(() => {
     if (updateSuccess) {
@@ -45,7 +141,6 @@ export const MessageUpdate = () => {
     }
   }, [updateSuccess]);
 
-  // eslint-disable-next-line complexity
   const saveEntity = values => {
     if (values.id !== undefined && typeof values.id !== 'number') {
       values.id = Number(values.id);
@@ -57,7 +152,10 @@ export const MessageUpdate = () => {
     const entity = {
       ...messageEntity,
       ...values,
-      senderProfile: userProfiles.find(it => it.id.toString() === values.senderProfile?.toString()),
+      //senderProfile: currentUserId, // Set senderProfile to the current user's ID
+      senderProfile: {
+        id: currentUserId, // Wrap currentUserId in an object
+      },
       receiverProfile: userProfiles.find(it => it.id.toString() === values.receiverProfile?.toString()),
     };
 
@@ -74,13 +172,15 @@ export const MessageUpdate = () => {
           sentAt: displayDefaultDateTime(),
           createdDate: displayDefaultDateTime(),
           lastModifiedDate: displayDefaultDateTime(),
+          senderProfile: currentUserId, // Set default senderProfile to current user ID
         }
       : {
           ...messageEntity,
           sentAt: convertDateTimeFromServer(messageEntity.sentAt),
           createdDate: convertDateTimeFromServer(messageEntity.createdDate),
           lastModifiedDate: convertDateTimeFromServer(messageEntity.lastModifiedDate),
-          senderProfile: messageEntity?.senderProfile?.id,
+          //senderProfile: currentUserId || messageEntity?.senderProfile?.id, // Use currentUserId or existing senderProfile ID
+          senderProfile: messageEntity?.senderProfile?.id, // Safely access senderProfile
           receiverProfile: messageEntity?.receiverProfile?.id,
         };
 
@@ -147,7 +247,7 @@ export const MessageUpdate = () => {
                 placeholder="YYYY-MM-DD HH:mm"
               /> */}
               {/* allows user to select other user as sender, logically should not happen. */}
-              <ValidatedField id="message-senderProfile" name="senderProfile" data-cy="senderProfile" label="Sender Profile" type="select">
+              {/* <ValidatedField id="message-senderProfile" name="senderProfile" data-cy="senderProfile" label="Sender Profile" type="select">
                 <option value="" key="0" />
                 {userProfiles
                   ? userProfiles.map(otherEntity => (
@@ -156,6 +256,19 @@ export const MessageUpdate = () => {
                       </option>
                     ))
                   : null}
+              </ValidatedField> */}
+              <ValidatedField
+                id="message-senderProfile"
+                name="senderProfile"
+                data-cy="senderProfile"
+                label="Sender Profile"
+                type="select"
+                value={currentUserId}
+                disabled // Sender should not be editable
+              >
+                <option value={currentUserId} key={currentUserId}>
+                  {userProfiles.find(profile => profile.user.id === currentUserId)?.user.login || 'Current User'}
+                </option>
               </ValidatedField>
               <ValidatedField
                 id="message-receiverProfile"
--- src/main/webapp/app/entities/message/message.tsx
+++ src/main/webapp/app/entities/message/message.tsx
@@ -1,31 +1,273 @@
+// import React, { useState, useEffect } from 'react';
+// import InfiniteScroll from 'react-infinite-scroll-component';
+// import { Link, useLocation, useNavigate } from 'react-router-dom';
+// import { Button, Table } from 'reactstrap';
+// import { byteSize, Translate, TextFormat, getPaginationState } from 'react-jhipster';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faSort, faSortUp, faSortDown } from '@fortawesome/free-solid-svg-icons';
+// import { APP_DATE_FORMAT, APP_LOCAL_DATE_FORMAT } from 'app/config/constants';
+// import { ASC, DESC, ITEMS_PER_PAGE, SORT } from 'app/shared/util/pagination.constants';
+// import { overridePaginationStateWithQueryParams } from 'app/shared/util/entity-utils';
+// import { useAppDispatch, useAppSelector } from 'app/config/store';
+
+// import { getEntities, reset } from './message.reducer';
+
+// export const Message = () => {
+//   const dispatch = useAppDispatch();
+
+//   const pageLocation = useLocation();
+
+//   const [paginationState, setPaginationState] = useState(
+//     overridePaginationStateWithQueryParams(getPaginationState(pageLocation, ITEMS_PER_PAGE, 'id'), pageLocation.search),
+//   );
+//   const [sorting, setSorting] = useState(false);
+
+//   const messageList = useAppSelector(state => state.message.entities);
+//   const loading = useAppSelector(state => state.message.loading);
+//   const links = useAppSelector(state => state.message.links);
+//   const updateSuccess = useAppSelector(state => state.message.updateSuccess);
+
+//   const getAllEntities = () => {
+//     dispatch(
+//       getEntities({
+//         page: paginationState.activePage - 1,
+//         size: paginationState.itemsPerPage,
+//         sort: `${paginationState.sort},${paginationState.order}`,
+//       }),
+//     );
+//   };
+
+//   const resetAll = () => {
+//     dispatch(reset());
+//     setPaginationState({
+//       ...paginationState,
+//       activePage: 1,
+//     });
+//     dispatch(getEntities({}));
+//   };
+
+//   useEffect(() => {
+//     resetAll();
+//   }, []);
+
+//   useEffect(() => {
+//     if (updateSuccess) {
+//       resetAll();
+//     }
+//   }, [updateSuccess]);
+
+//   useEffect(() => {
+//     getAllEntities();
+//   }, [paginationState.activePage]);
+
+//   const handleLoadMore = () => {
+//     if ((window as any).pageYOffset > 0) {
+//       setPaginationState({
+//         ...paginationState,
+//         activePage: paginationState.activePage + 1,
+//       });
+//     }
+//   };
+
+//   useEffect(() => {
+//     if (sorting) {
+//       getAllEntities();
+//       setSorting(false);
+//     }
+//   }, [sorting]);
+
+//   const sort = p => () => {
+//     dispatch(reset());
+//     setPaginationState({
+//       ...paginationState,
+//       activePage: 1,
+//       order: paginationState.order === ASC ? DESC : ASC,
+//       sort: p,
+//     });
+//     setSorting(true);
+//   };
+
+//   const handleSyncList = () => {
+//     resetAll();
+//   };
+
+//   const getSortIconByFieldName = (fieldName: string) => {
+//     const sortFieldName = paginationState.sort;
+//     const order = paginationState.order;
+//     if (sortFieldName !== fieldName) {
+//       return faSort;
+//     } else {
+//       return order === ASC ? faSortUp : faSortDown;
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <h2 id="message-heading" data-cy="MessageHeading">
+//         Messages
+//         <div className="d-flex justify-content-end">
+//           <Button className="me-2" color="info" onClick={handleSyncList} disabled={loading}>
+//             <FontAwesomeIcon icon="sync" spin={loading} /> Refresh list
+//           </Button>
+//           <Link to="/message/new" className="btn btn-primary jh-create-entity" id="jh-create-entity" data-cy="entityCreateButton">
+//             <FontAwesomeIcon icon="plus" />
+//             &nbsp; Create a new Message
+//           </Link>
+//         </div>
+//       </h2>
+//       <div className="table-responsive">
+//         <InfiniteScroll
+//           dataLength={messageList ? messageList.length : 0}
+//           next={handleLoadMore}
+//           hasMore={paginationState.activePage - 1 < links.next}
+//           loader={<div className="loader">Loading ...</div>}
+//         >
+//           {messageList && messageList.length > 0 ? (
+//             <Table responsive>
+//               <thead>
+//                 <tr>
+//                   <th className="hand" onClick={sort('id')}>
+//                     ID <FontAwesomeIcon icon={getSortIconByFieldName('id')} />
+//                   </th>
+//                   <th className="hand" onClick={sort('content')}>
+//                     Content <FontAwesomeIcon icon={getSortIconByFieldName('content')} />
+//                   </th>
+//                   <th className="hand" onClick={sort('sentAt')}>
+//                     Sent At <FontAwesomeIcon icon={getSortIconByFieldName('sentAt')} />
+//                   </th>
+//                   <th className="hand" onClick={sort('isDeleted')}>
+//                     Is Deleted <FontAwesomeIcon icon={getSortIconByFieldName('isDeleted')} />
+//                   </th>
+//                   <th className="hand" onClick={sort('createdBy')}>
+//                     Created By <FontAwesomeIcon icon={getSortIconByFieldName('createdBy')} />
+//                   </th>
+//                   <th className="hand" onClick={sort('createdDate')}>
+//                     Created Date <FontAwesomeIcon icon={getSortIconByFieldName('createdDate')} />
+//                   </th>
+//                   <th className="hand" onClick={sort('lastModifiedBy')}>
+//                     Last Modified By <FontAwesomeIcon icon={getSortIconByFieldName('lastModifiedBy')} />
+//                   </th>
+//                   <th className="hand" onClick={sort('lastModifiedDate')}>
+//                     Last Modified Date <FontAwesomeIcon icon={getSortIconByFieldName('lastModifiedDate')} />
+//                   </th>
+//                   <th>
+//                     Sender Profile <FontAwesomeIcon icon="sort" />
+//                   </th>
+//                   <th>
+//                     Receiver Profile <FontAwesomeIcon icon="sort" />
+//                   </th>
+//                   <th />
+//                 </tr>
+//               </thead>
+//               <tbody>
+//                 {messageList.map((message, i) => (
+//                   <tr key={`entity-${i}`} data-cy="entityTable">
+//                     <td>
+//                       <Button tag={Link} to={`/message/${message.id}`} color="link" size="sm">
+//                         {message.id}
+//                       </Button>
+//                     </td>
+//                     <td>{message.content}</td>
+//                     <td>{message.sentAt ? <TextFormat type="date" value={message.sentAt} format={APP_DATE_FORMAT} /> : null}</td>
+//                     <td>{message.isDeleted ? 'true' : 'false'}</td>
+//                     <td>{message.createdBy}</td>
+//                     <td>{message.createdDate ? <TextFormat type="date" value={message.createdDate} format={APP_DATE_FORMAT} /> : null}</td>
+//                     <td>{message.lastModifiedBy}</td>
+//                     <td>
+//                       {message.lastModifiedDate ? (
+//                         <TextFormat type="date" value={message.lastModifiedDate} format={APP_DATE_FORMAT} />
+//                       ) : null}
+//                     </td>
+//                     <td>
+//                       {message.senderProfile ? (
+//                         <Link to={`/user-profile/${message.senderProfile.id}`}>{message.senderProfile.id}</Link>
+//                       ) : (
+//                         ''
+//                       )}
+//                     </td>
+//                     <td>
+//                       {message.receiverProfile ? (
+//                         <Link to={`/user-profile/${message.receiverProfile.id}`}>{message.receiverProfile.id}</Link>
+//                       ) : (
+//                         ''
+//                       )}
+//                     </td>
+//                     <td className="text-end">
+//                       <div className="btn-group flex-btn-group-container">
+//                         <Button tag={Link} to={`/message/${message.id}`} color="info" size="sm" data-cy="entityDetailsButton">
+//                           <FontAwesomeIcon icon="eye" /> <span className="d-none d-md-inline">View</span>
+//                         </Button>
+//                         <Button tag={Link} to={`/message/${message.id}/edit`} color="primary" size="sm" data-cy="entityEditButton">
+//                           <FontAwesomeIcon icon="pencil-alt" /> <span className="d-none d-md-inline">Edit</span>
+//                         </Button>
+//                         <Button
+//                           onClick={() => (window.location.href = `/message/${message.id}/delete`)}
+//                           color="danger"
+//                           size="sm"
+//                           data-cy="entityDeleteButton"
+//                         >
+//                           <FontAwesomeIcon icon="trash" /> <span className="d-none d-md-inline">Delete</span>
+//                         </Button>
+//                       </div>
+//                     </td>
+//                   </tr>
+//                 ))}
+//               </tbody>
+//             </Table>
+//           ) : (
+//             !loading && <div className="alert alert-warning">No Messages found</div>
+//           )}
+//         </InfiniteScroll>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Message;
+
 import React, { useState, useEffect } from 'react';
 import InfiniteScroll from 'react-infinite-scroll-component';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { Button, Table } from 'reactstrap';
 import { byteSize, Translate, TextFormat, getPaginationState } from 'react-jhipster';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSort, faSortUp, faSortDown } from '@fortawesome/free-solid-svg-icons';
-import { APP_DATE_FORMAT, APP_LOCAL_DATE_FORMAT } from 'app/config/constants';
-import { ASC, DESC, ITEMS_PER_PAGE, SORT } from 'app/shared/util/pagination.constants';
+import { APP_DATE_FORMAT } from 'app/config/constants';
+import { ASC, DESC, ITEMS_PER_PAGE } from 'app/shared/util/pagination.constants';
 import { overridePaginationStateWithQueryParams } from 'app/shared/util/entity-utils';
 import { useAppDispatch, useAppSelector } from 'app/config/store';
-
 import { getEntities, reset } from './message.reducer';
+import axios from 'axios';
 
 export const Message = () => {
   const dispatch = useAppDispatch();
-
   const pageLocation = useLocation();
 
   const [paginationState, setPaginationState] = useState(
     overridePaginationStateWithQueryParams(getPaginationState(pageLocation, ITEMS_PER_PAGE, 'id'), pageLocation.search),
   );
   const [sorting, setSorting] = useState(false);
+  const [currentUserId, setCurrentUserId] = useState(null);
 
   const messageList = useAppSelector(state => state.message.entities);
   const loading = useAppSelector(state => state.message.loading);
   const links = useAppSelector(state => state.message.links);
   const updateSuccess = useAppSelector(state => state.message.updateSuccess);
+
+  // Fetch current user ID
+  useEffect(() => {
+    const fetchCurrentUserId = async () => {
+      try {
+        const response = await axios.get('/api/current-user');
+        setCurrentUserId(response.data);
+      } catch (error) {
+        console.error('Error fetching current user ID:', error);
+      }
+    };
+
+    fetchCurrentUserId();
+    resetAll();
+  }, []);
 
   const getAllEntities = () => {
     dispatch(
@@ -45,10 +287,6 @@ export const Message = () => {
     });
     dispatch(getEntities({}));
   };
-
-  useEffect(() => {
-    resetAll();
-  }, []);
 
   useEffect(() => {
     if (updateSuccess) {
@@ -197,7 +435,15 @@ export const Message = () => {
                         <Button tag={Link} to={`/message/${message.id}`} color="info" size="sm" data-cy="entityDetailsButton">
                           <FontAwesomeIcon icon="eye" /> <span className="d-none d-md-inline">View</span>
                         </Button>
-                        <Button tag={Link} to={`/message/${message.id}/edit`} color="primary" size="sm" data-cy="entityEditButton">
+                        {/* Conditionally disable Edit and Delete buttons */}
+                        <Button
+                          tag={Link}
+                          to={`/message/${message.id}/edit`}
+                          color="primary"
+                          size="sm"
+                          data-cy="entityEditButton"
+                          disabled={currentUserId === message.receiverProfile?.id} // Disable if current user is receiver
+                        >
                           <FontAwesomeIcon icon="pencil-alt" /> <span className="d-none d-md-inline">Edit</span>
                         </Button>
                         <Button
@@ -205,6 +451,7 @@ export const Message = () => {
                           color="danger"
                           size="sm"
                           data-cy="entityDeleteButton"
+                          disabled={currentUserId === message.receiverProfile?.id} // Disable if current user is receiver
                         >
                           <FontAwesomeIcon icon="trash" /> <span className="d-none d-md-inline">Delete</span>
                         </Button>
